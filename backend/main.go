d586494c9dff433dadc17320a24cea217e7ffc12
Varis <varisnithiparkorn@gmail.com>
2025-11-04 14:45:58 +0700

    update using port and dockerfile

--- backend/main.go
+++ backend/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"log"
 
+	"github.com/VarisNithiparkorn/cryptoGraph/config"
 	"github.com/VarisNithiparkorn/cryptoGraph/routers"
 )
 
 
 func main() {
-
+	port := config.LoadEnv().Port
 	 router := routers.SetUpRouter()
-	 err := router.Run(":8080") 
+	 err := router.Run(":"+ port) 
      if err != nil {
          log.Fatalf("Server failed to start: %v", err)
      }
