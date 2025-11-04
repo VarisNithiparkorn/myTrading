96671af563d4f29777e85ef311f28affc7cc762b
Varis <varisnithiparkorn@gmail.com>
2025-11-04 22:29:27 +0700

    add send email feature

--- frontend/app/utils/fetchUtils.ts
+++ frontend/app/utils/fetchUtils.ts
@@ -1,11 +1,15 @@
 import { json } from "stream/consumers"
 import { Account } from "./interface"
 
-const url:string = process.env.BACKEND_URL ?? "http://localhost:8080/"
+const url:string = process.env.BACKEND_URL ?? "http://localhost:5050/"
 export const register = async(account:Account)=>{
+    try{
     const res:Response = await fetch(url+"api/v1/register",{
         method:"POST",
         body:JSON.stringify(account)
     })
     return res.status
+    }catch(e){
+        console.log("register faile")
+    }
 }
